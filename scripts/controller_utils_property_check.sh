2e2afef5a121c61ed6bf2f3c37b515614d811b2b
Rob Frawley 2nd <rfrawley@scribenet.com>
2015-06-15 22:29:47 -0400

    updated icon data

--- scripts/controller_utils_property_check.sh
+++ scripts/controller_utils_property_check.sh
@@ -1,1 +1,2 @@
-grep -r '$this->utils' {src,lib}/**/**/Controller/*.php | sed -r 's/.*(this->utils->[A-Za-z]+).*/\1/g' | sort | uniq
+#grep -r '$this->utils' {src,lib}/**/**/Controller/*.php | sed -r 's/.*(this->utils->[A-Za-z]+).*/\1/g' | sort | uniq
+grep -r '$this->utils' {src,lib}/**/**/Controller/*.php | sed -r 's/.*(this->utils->[A-Za-z]+).*/\1/g'
