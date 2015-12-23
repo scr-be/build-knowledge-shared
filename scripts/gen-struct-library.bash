32b0b79e5352aa167b1f3d54a457ca3759ebfdd4
Rob Frawley 2nd <rmf@src.run>
2015-12-23 05:22:16 -0500

    gen skel updates

--- scripts/gen-struct-library.bash
+++ scripts/gen-struct-library.bash
@@ -3,4 +3,4 @@
 cp $(dirname $0)/tpl-gitignore.txt $(pwd)/.gitignore
 cp $(dirname $0)/tpl-contributing.md $(pwd)/CONTRIBUTING.md
 cp $(dirname $0)/tpl-license.md $(pwd)/LICENSE.md
-./$(dirname $0)/gen-struct.php tpl-readme-bundle.md
+./$(dirname $0)/gen-struct.php tpl-readme-library.md
