86b59b2296a708379a81f227ea0779ac25b1a642
Rob Frawley 2nd <rmf@src.run>
2016-01-03 09:28:49 -0500

    updated db create for mantle

--- builders/_inc-app-prep/app-prep_db-create-mantle.bash
+++ builders/_inc-app-prep/app-prep_db-create-mantle.bash
@@ -11,12 +11,13 @@
 
 RT_COMMANDS_ACT=(
     "bash ${DIR_CWD}/db-create-mantle.sh"
+    "rm -fr ${DIR_CWD}/db-create-mantle.sh ${DIR_CWD}/create.sql ${DIR_CWD}/create1.sql ${DIR_CWD}/create2.sql"
 )
 
 echo "${APP_MAKE_CLI} doctrine:database:create -n && \\" > "${DIR_CWD}/db-create-mantle.sh"
-echo "${APP_MAKE_CLI} doctrine:schema:update --dump-sql > create.sql && \\" >> "${DIR_CWD}/db-create-mantle.sh"
-echo "sed 's/ utf8 / utf8mb4 /g' -- create.sql > create1.sql && \\" >> "${DIR_CWD}/db-create-mantle.sh"
-echo "sed 's/ utf8_unicode_ci / utf8mb4_unicode_ci /g' -- create1.sql > create2.sql && \\" >> "${DIR_CWD}/db-create-mantle.sh"
-echo "mysql --verbose -uroot Test_ScribeMantleBundle < create2.sql" >> "${DIR_CWD}/db-create-mantle.sh"
+echo "${APP_MAKE_CLI} doctrine:schema:update --dump-sql > ${DIR_CWD}/create.sql && \\" >> "${DIR_CWD}/db-create-mantle.sh"
+echo "sed 's/ utf8 / utf8mb4 /g' -- ${DIR_CWD}/create.sql > ${DIR_CWD}/create1.sql && \\" >> "${DIR_CWD}/db-create-mantle.sh"
+echo "sed 's/ utf8_unicode_ci / utf8mb4_unicode_ci /g' -- ${DIR_CWD}/create1.sql > ${DIR_CWD}/create2.sql && \\" >> "${DIR_CWD}/db-create-mantle.sh"
+echo "mysql --verbose -uroot Test_ScribeMantleBundle < ${DIR_CWD}/create2.sql" >> "${DIR_CWD}/db-create-mantle.sh"
 
 # EOF #
