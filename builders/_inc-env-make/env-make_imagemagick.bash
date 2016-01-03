624399099249a748e5fc25e89b4eaeaf78d49eab
Rob Frawley 2nd <rmf@src.run>
2016-01-03 02:58:04 -0500

    updated for imagick

--- builders/_inc-env-make/env-make_imagemagick.bash
+++ builders/_inc-env-make/env-make_imagemagick.bash
@@ -17,16 +17,17 @@ SYS_REQ="$(basename ${SCRIPT_SELF_BASE:14} .bash)"
 
 type outLines &>> /dev/null || . ${SCRIPT_SELF_RPATH}/../_common/bash-inc_all.bash
 
-${CMD_PRE} apt-get install build-essential;
-sudo apt-get build-dep imagemagick;
-sudo apt-get install libautotrace3 libautotrace-dev autotrace ghostscript gsfonts libgs-dev pslib1 libopenjpeg-dev libopenjp2-7-dev libjxr-dev libhpdf-dev libfreeimage-dev libwebp-dev libexif-dev libjasper-dev libjpeg-dev libjpeg-turbo8-dev libjpeg8-dev libgd-dev libpng12-dev libpng++-dev libpnglite-dev libfftw3-dev libfftw3-quad3 libgxps-dev libgxps-utils liblcms2-dev liblcms2-utils gir1.2-rsvg-2.0 libgc1c2 libgraphviz-dev libgsl0ldbl libgtkspell0 liblqr-1-0-dev libnetpbm10 libplot-dev libplot2c2 librsvg2-dev libwmf-bin libwmf-dev libxaw7-dev libxdot4 libxmu-dev libxmu-headers netpbm checkinstall automake autoconf libtool pkg-config checkinstall;
-
-wget http://www.imagemagick.org/download/ImageMagick.tar.bz2
-tar xjf ImageMagick-*.tar.bz2 && cd ImageMagick-*/
-
-./configure CXXFLAGS="-march=native -g -O2" CPPFLAGS="-march=native -g -O2" --with-autotrace --with-webp --with-rsvg --with-gslib --with-jbig --with-jpeg --with-jp2 --enable-hdri --with-quantum-depth=32 --with-modules --with-fftw --with-perl
-make
-
-/sbin/ldconfig /usr/local/lib
+#${CMD_PRE} apt-get install build-essential;
+#${CMD_PRE} apt-get build-dep imagemagick;
+#${CMD_PRE} apt-get install libautotrace3 libautotrace-dev autotrace ghostscript gsfonts libgs-dev pslib1 libopenjpeg-dev libopenjp2-7-dev libjxr-dev libhpdf-dev libfreeimage-dev libwebp-dev libexif-dev libjasper-dev libjpeg-dev libjpeg-turbo8-dev libjpeg8-dev libgd-dev libpng12-dev libpng++-dev libpnglite-dev libfftw3-dev libfftw3-quad3 libgxps-dev libgxps-utils liblcms2-dev liblcms2-utils gir1.2-rsvg-2.0 libgc1c2 libgraphviz-dev libgsl0ldbl libgtkspell0 liblqr-1-0-dev libnetpbm10 libplot-dev libplot2c2 librsvg2-dev libwmf-bin libwmf-dev libxaw7-dev libxdot4 libxmu-dev libxmu-headers netpbm checkinstall automake autoconf libtool pkg-config checkinstall;
+#
+#rm -fr ImageMagick*
+#wget http://www.imagemagick.org/download/ImageMagick.tar.bz2
+#tar xjf ImageMagick-*.tar.bz2 && cd ImageMagick-*/
+#
+#./configure CXXFLAGS="-march=native -g -O2" CPPFLAGS="-march=native -g -O2" --with-autotrace --with-webp --with-rsvg --with-gslib --with-jbig --with-jpeg --with-jp2 --enable-hdri --with-quantum-depth=32 --with-modules --with-fftw --with-perl
+#make
+#
+#/sbin/ldconfig /usr/local/lib
 
 # EOF #
